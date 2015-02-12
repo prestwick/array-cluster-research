448a0def80c69e8b491df16068ef9f139e69d003
Mark Black <mark.prestwick@gmail.com>
2015-02-12 16:25:50 -0600

    Add/remove widgets and rows
    Added some buttons to add and remove sliders.  Added button to add and
    remove rows.  Consolidated how widget initialization occurs.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,18 @@
-/*global $, document */
+/*global $, document, console */
 
 $(document).ready(function () {
     'use strict';
-    var sliderCtl,
-        numericCtl,
-        stringCtl,
+    var sliderCtl = $('.sliderCtl').jqxSlider(),
+        numericCtl = $('.numericCtl').jqxNumberInput({ width: '250px', height: '25px',  spinButtons: true }),
+        stringCtl = $('.stringCtl').jqxInput({placeHolder: 'Placeholder Text', height: 25, width: 200, minLength: 1}),
         dataGrid = $('#dataGrid'),
+        addWidget = $('#addWidget').jqxButton({width: '150'}),
+        removeWidget = $('#removeWidget').jqxButton({width: '150'}),
+        addRowButton = $('#addRowButton').jqxButton({width: '150'}),
+        removeRowButton = $('#removeRowButton').jqxButton({width: '150'}),
         dataAdapter,
         data = [0, 0, 0, 0],
         source,
-        rowHtml,
         cellRenderer;
     
     source = {
@@ -18,11 +21,6 @@ $(document).ready(function () {
     };
     
     cellRenderer = function () {
-//        return '<div class="cluster">' +
-//                '<div class="sliderCtl"></div>' +
-//                '<div class="numericCtl"></div>' +
-//                '<input type="text" class="stringCtl"/>' +
-//                '</div>';
         return $('.cluster').html();
     };
     
@@ -34,13 +32,40 @@ $(document).ready(function () {
         columns : [
             {text: 'column', datafield: 'column', width: 350, cellsrenderer: cellRenderer}
         ],
-        rowsheight: 100,
-        autoheight: true
+        rowsheight: 150,
+        autoheight: true,
+        rendered: function () {
+            //there are cases where there may be no sliders present. 
+            try {
+                sliderCtl = $('.sliderCtl').jqxSlider();
+            } catch (err) {
+                console.log(err);
+            }
+            numericCtl = $('.numericCtl').jqxNumberInput({ width: '250px', height: '25px',  spinButtons: true });
+            stringCtl = $('.stringCtl').jqxInput({placeHolder: 'Placeholder Text', height: 25, width: 200, minLength: 1});
+        }
     });
-    sliderCtl = $('.sliderCtl').jqxSlider();
-    numericCtl = $('.numericCtl').jqxNumberInput({ width: '250px', height: '25px',  spinButtons: true });
-    stringCtl = $('.stringCtl').jqxInput({placeHolder: 'Placeholder Text', height: 25, width: 200, minLength: 1});
+
+    addWidget.on('click', function () {
+        var sliderHtml = '<div class="sliderCtl"></div>';
+        $('.cluster').prepend(sliderHtml);
+        dataGrid.jqxGrid('render');
+    });
+    
+    removeWidget.on('click', function () {
+        //remove the slider widget
+        sliderCtl.remove();
+        dataGrid.jqxGrid('render');
+    });
+    
+    addRowButton.on('click', function () {
+        dataGrid.jqxGrid('addrow', null, {});
+    });
+    
+    removeRowButton.on('click', function () {
+        dataGrid.jqxGrid('deleterow', dataGrid.jqxGrid('getrows').length - 1);
+    });
 });
-//what if we want to add or remove an element of the cluster
+//what if we want to add or remove an element of the cluster - Done
 //what if we have nested clusters
 //how would we build this up dynamically. 
