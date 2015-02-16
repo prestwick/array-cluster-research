a6a888c7879ea84f7c2a09414b3c71e9474c5908
Mark Black <mark.prestwick@gmail.com>
2015-02-16 10:16:57 -0600

    Array of Clusters complete
    Finished demo functionality.  Code clean-up and comments.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,71 +1,96 @@
 /*global $, document, console */
 
+// This demonstrates how an array of clusters can be composed using a template HTML cluster and the jqxGrid. 
+// All manipulation of the cluster within the grid occures in a hidden <div> that represents the cluster. 
+// Only the widgets within the cluster within the grid are initialized. 
+
 $(document).ready(function () {
     'use strict';
-    var sliderCtl = $('.sliderCtl').jqxSlider(),
-        numericCtl = $('.numericCtl').jqxNumberInput({ width: '250px', height: '25px',  spinButtons: true }),
-        stringCtl = $('.stringCtl').jqxInput({placeHolder: 'Placeholder Text', height: 25, width: 200, minLength: 1}),
+    
+    // Setup scope variables
+    var source,
+        dataAdapter,
+        data = [0, 0, 0, 0],
+        cellRenderer,
         dataGrid = $('#dataGrid'),
+        clusterTemplate = $('.cluster-template'),
         addWidget = $('#addWidget').jqxButton({width: '150'}),
         removeWidget = $('#removeWidget').jqxButton({width: '150'}),
         addRowButton = $('#addRowButton').jqxButton({width: '150'}),
-        removeRowButton = $('#removeRowButton').jqxButton({width: '150'}),
-        dataAdapter,
-        data = [0, 0, 0, 0],
-        source,
-        cellRenderer;
-    
+        removeRowButton = $('#removeRowButton').jqxButton({width: '150'});
+        
+    // Setup source object for jqx data adapter
     source = {
         localdata: data,
         datatype: 'array'
     };
-    
-    cellRenderer = function () {
-        return $('.cluster').html();
-    };
-    
     dataAdapter = new $.jqx.dataAdapter(source, {});
     
+    
+    /**     * Function to populate each grid (array) cell with the HTML of the cluster of widgets.
+            * @returns {html} Grid cell HTML     
+    */
+    cellRenderer = function () {
+        console.log('cell render called');
+        return clusterTemplate.html();
+    };
+    
+    // Setup dataGrid
     dataGrid.jqxGrid({
-        width: 350,
+        width: 300,
         source: dataAdapter,
+        
+        // Create a single column who uses the cellRenderer function to render each grid cell. 
         columns : [
-            {text: 'column', datafield: 'column', width: 350, cellsrenderer: cellRenderer}
+            {text: 'column', datafield: 'column', width: 300, cellsrenderer: cellRenderer}
         ],
         rowsheight: 150,
         autoheight: true,
+        
+        // Rendered function initializes the jqxWidgets from the raw cluster HTML. 
+        // Try catch is required since this can be called before cluster HTML is inserted into each cell.  
         rendered: function () {
-            //there are cases where there may be no sliders present. 
+            console.log('rendered called');
             try {
-                sliderCtl = $('.sliderCtl').jqxSlider();
-            } catch (err) {
-                console.log(err);
+                dataGrid.find('.sliderCtl').jqxSlider();
+            } catch (errSliderRender) {
+                console.log(errSliderRender);
             }
-            numericCtl = $('.numericCtl').jqxNumberInput({ width: '250px', height: '25px',  spinButtons: true });
-            stringCtl = $('.stringCtl').jqxInput({placeHolder: 'Placeholder Text', height: 25, width: 200, minLength: 1});
+            try {
+                dataGrid.find('.stringCtl').jqxInput({placeHolder: 'Placeholder Text', height: 25, width: 200, minLength: 1});
+            } catch (errStringRender) {
+                console.log(errStringRender);
+            }
+            try {
+                dataGrid.find('.numericCtl').jqxNumberInput({ width: '250px', height: '25px',  spinButtons: true });
+            } catch (errNumericRender) {
+                console.log(errNumericRender);
+            }
         }
     });
 
+    // Button to add additional sliders to the cluster HTML
     addWidget.on('click', function () {
         var sliderHtml = '<div class="sliderCtl"></div>';
-        $('.cluster').prepend(sliderHtml);
+        clusterTemplate.prepend(sliderHtml);
         dataGrid.jqxGrid('render');
     });
     
+    // Button to remove all slider widgets from the cluster HTML
     removeWidget.on('click', function () {
-        //remove the slider widget
-        sliderCtl.remove();
+        clusterTemplate.find('.sliderCtl').remove();
         dataGrid.jqxGrid('render');
     });
     
+    // Add a row to the data grid (array)
     addRowButton.on('click', function () {
         dataGrid.jqxGrid('addrow', null, {});
+        dataGrid.jqxGrid('render');
     });
     
+    // Removes the button row of the data grid (array)
     removeRowButton.on('click', function () {
         dataGrid.jqxGrid('deleterow', dataGrid.jqxGrid('getrows').length - 1);
+        dataGrid.jqxGrid('render');
     });
 });
-//what if we want to add or remove an element of the cluster - Done
-//what if we have nested clusters
-//how would we build this up dynamically. 
